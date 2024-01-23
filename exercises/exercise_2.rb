241d2f29d6238412270fd71c30929ccc7fb9ddb4
joycuicui <cuiqijoy@outlook.com>
2024-01-22 17:49:34 -0800

    ex2: update first store's name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# @store1 = Store.find_by(id: 1)
+@store1 = Store.find(1)
+@store1.update(name: 'New Burnaby')
+
+@store2 = Store.find(2)
+
+puts "Store 1's name is now #{@store1.name}"
